b3b13817508b83719be1d41ddebb6173edf1cf5a
Ingrid Emanuelle <ingrid.engdesign@outlook.com>
2025-06-30 21:39:42 -0300

    criacao de repositories

---
+++ src/main/java/com/deliverytech/delivery_api/repository/PedidoRepository.java
@@ -0,0 +1,20 @@
+package com.deliverytech.delivery_api.repository;
+
+import com.deliverytech.delivery_api.entity.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+    List<Pedido> findByClienteId(Long clienteId);
+
+    List<Pedido> findByStatus(String status);
+
+    List<Pedido> findTop10ByOrderByDataPedidoDesc();
+
+    List<Pedido> findByDataPedidoBetween(LocalDateTime inicio, LocalDateTime fim);
+}
